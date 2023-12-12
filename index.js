32889eb1d65c5c1b53833d3bfa32513d3e2395a8
zqyoiv <93994267+zqyoiv@users.noreply.github.com>
2023-12-12 17:43:22 -0500

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/start_record', (req, res) => {
+    io.emit('start_record');
+    res.send('Start recording signal sent to all clients');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ public/sketch.js
@@ -0,0 +1,195 @@
+let recording = false;
+let chunks = [];
+let capture;
+let socket;
+
+let canvas;
+let cv;
+let start = false
+let loadVideo = false
+let recorder, stream, recordedChunks = [];
+let video;
+let isRecording = false;
+let isPlaying = false;
+
+function setup() {
+  cv = createCanvas(windowWidth, windowHeight)
+  cv.hide()
+  frameRate(30);
+
+  let hiddenCanvas = document.getElementById('hiddenCanvas');
+  let hiddenContext = hiddenCanvas.getContext('2d');
+  hiddenCanvas.width = windowWidth;
+  hiddenCanvas.height = windowHeight;
+
+  hiddenContext.fillStyle = 'black';
+  hiddenContext.fillRect(0, 0, hiddenCanvas.width, hiddenCanvas.height);
+
+  capture = createCapture(VIDEO);
+  capture.size(640, 480);
+  capture.hide();
+  background(0);
+  frameRate(30);
+
+  let stream = cv.elt.captureStream(30);
+  recorder = new MediaRecorder(stream, {
+    mimeType: 'video/webm; codecs=vp9'
+  });
+
+  recorder.ondataavailable = e => {
+    if (e.data.size > 0) {
+      recordedChunks.push(e.data);
+    }
+  };
+
+  socket = io.connect(window.location.origin);
+
+  socket.on('start_record', function() {
+    switchState();
+    console.log('Start recording signal received');
+    // Add your code here to handle the start recording event
+  });
+
+  socket.on('stop_and_play', function() {
+    switchState();
+    console.log('Stop and play signal received');
+    // Add your code here to handle the start recording event
+  });
+}
+
+function draw() {
+  if (start) {
+    if (!isPlaying) {
+      capture.loadPixels();
+      for (let i = 0; i < 8000; i++) {
+        let getX = random(capture.width);
+        let getY = random(capture.height);
+        let index = (int(getY) * capture.width + int(getX)) * 4;
+        let r = capture.pixels[index];
+        let g = capture.pixels[index + 1];
+        let b = capture.pixels[index + 2];
+
+        // 转换为灰度
+        let gray = (r + g + b) / 3;
+
+        // 如果灰度非常低（接近黑色），替换为蓝色，否则为白色
+        if (gray < 120) { // 调整这个阈值来决定何种灰度级别被视为“最黑”
+          r = 10;
+          g = 10;
+          b =10;
+
+        } else if (gray < 70) {
+          r = 50;
+          g = 50;
+          b = 50;
+        }
+
+        else {
+          r = 220;
+          g = 220
+          b = 220;
+        }
+
+        let col = color(r, g, b, 100);
+
+        let x = map(getX, 0, 640, 0, width);
+        let y = map(getY, 0, 480, 0, height);
+        let length = map(brightness(col), 0, 255, 10, 120);
+        let angle = map(brightness(col), 0, 255, 0, PI);
+        noStroke();
+        fill(col, 127);
+        push();
+        translate(x, y);
+        rectMode(CENTER);
+        rotate(angle);
+        ellipse(0, 0, length, 2);
+        pop();
+      }
+
+      if (recordedChunks.length > 0 ) {
+        let videoBlob = new Blob(recordedChunks, {
+          type: 'video/webm'
+        });
+        video = createVideo(URL.createObjectURL(videoBlob), videoLoaded);
+        video.hide(); // 隐藏 HTML 视频元素
+        isPlaying = true;
+        loadVideo = false;
+      }
+
+    } else {
+      if(video){
+        image(video, 0, 0, width, height);
+      }
+    }
+
+   if (isRecording) {
+      hiddenCanvas.style.display = 'block'; // 显示隐藏画布
+      cv.hide()
+    } else {
+      hiddenCanvas.style.display = 'none'; // 显示隐藏画布
+      cv.show()
+    }  
+  }else{
+    hiddenCanvas.style.display = 'block'; // 显示隐藏画布
+    cv.hide()
+  }
+  console.log("record:"+isRecording,"playing:"+isPlaying)
+  
+}
+
+function enhanceContrast(vid) {
+  const contrastFactor = (259 * (128 + 255)) / (255 * (259 - 128)); // 对比度因子
+
+  for (let i = 0; i < vid.pixels.length; i += 4) {
+    vid.pixels[i] = truncateColor(contrastFactor * (vid.pixels[i] - 128) + 128);
+    vid.pixels[i + 1] = truncateColor(contrastFactor * (vid.pixels[i + 1] - 128) + 128);
+    vid.pixels[i + 2] = truncateColor(contrastFactor * (vid.pixels[i + 2] - 128) + 128);
+  }
+}
+
+function truncateColor(value) {
+  return min(max(value, 0), 255);
+}
+
+function keyPressed() {
+
+  if (key === 'r' || key === 'R') {
+    start = true
+    // 按 P 开始播放录制的视频
+  
+    // 按 R 开始或停止录制
+    if (recorder.state === 'inactive') {
+      recordedChunks = []; // 清空之前的录制
+      recorder.start();
+      isRecording = true;
+      isPlaying = false;
+      console.log('Recording started');
+    } else {
+      recorder.stop();
+      isRecording = false;
+      console.log('Recording stopped');
+      loadVideo = true
+    }    
+  }
+}
+
+function switchState() {
+    start = true;
+    if (recorder.state === 'inactive') {
+      recordedChunks = []; // 清空之前的录制
+      recorder.start();
+      isRecording = true;
+      isPlaying = false;
+      console.log('Recording started');
+    } else {
+      recorder.stop();
+      isRecording = false;
+      console.log('Recording stopped');
+      loadVideo = true
+    }
+}
+
+function videoLoaded() {
+  video.loop(); // 设置视频循环播放
+  video.volume(0); // 如果不需要音频，可以将音量设置为0
+}
